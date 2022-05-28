912b0f9c180e7de458169f4229d080c534d07123
Sharlyn Lyman <slyman13.sl@gmail.com>
2022-05-28 12:26:27 -0700

    2nd commit today

--- src/Cards/CardAdd.js
+++ src/Cards/CardAdd.js
@@ -5,6 +5,7 @@ import {
   useHistory,
 } from "react-router-dom";
 import { createCard, readDeck } from "../utils/api";
+import CardForm from "./CardForm";
 
 ////CardEdit and CardAdd share the same components
 //You must use the readDeck() function from src/utils/api/index.js to load the deck that you're adding the card to.
@@ -52,6 +53,14 @@ function CardAdd () {
     const cardStyle = {
         marginRight: "5px",
     }
+
+    /* There is a breadcrumb navigation bar with a link to home /, 
+    followed by the name of the deck to which the cards are being added,
+     and finally the text Add Card (e.g., Home/React Router/Add Card).
+     The screen displays the React Router: Add Card deck title.A form is shown with the "front" and "back" fields 
+     for a new card. Both fields use a <textarea> tag that can accommodate multiple lines of text.
+     If the user clicks Save, a new card is created and associated with the relevant deck. 
+    Then the form is cleared and the process for adding a card is restarted. */
 
     return (
         <div>
